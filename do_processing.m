8aaecec6c2ba55b7655c20d873cbf9d8e9eb9102
Jeremy Magland <jeremy.magland@gmail.com>
2015-10-30 21:05:13 -0400

    info

--- do_processing.m
+++ do_processing.m
@@ -2,13 +2,16 @@ function do_processing
 
 %close all;
 
+%Add some necessary paths
 addpath('processing');
 addpath('util');
 addpath('isosplit');
 
+%Create some directories
 if (~exist('output','dir')) mkdir('output'); end;
 if (~exist('output/detect','dir')) mkdir('output/detect'); end;
 
+%Set some options and specify the input/output file names
 opts.verbose=1;
 opts.raw_dat='raw/ms11d45.dat';
 opts.channels=[37:52,68,69];
@@ -22,10 +25,17 @@ opts.locations='raw/locations.mda';
 opts.adjacency='raw/adjacency.mda';
 opts.num_cluster_features=3;
 
+%Preprocess
 data=step1_preprocess(opts);
 step1a_prepare(opts);
+
+%Detect
 step2_detect(opts,data);
+
+%Cluster
 step3_cluster(opts,data);
+
+%Final fitting stage needed
 
 end
 
